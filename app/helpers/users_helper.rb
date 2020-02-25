122b13563c239625b77c50df587df897f81a3207
Jesse Novotny <jessenovotny@gmail.com>
2020-02-25 14:42:42 -0700

    move user.active? to helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,9 @@
 module UsersHelper
   def status_color(user)
-    {style: 'background-color:' + (user.active? ? 'green;' : 'red;')}
+    {style: 'background-color:' + (active?(user) ? 'green;' : 'red;')}
+  end
+
+  def active?(user)
+    user.status == 'active'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,4 @@ class User < ApplicationRecord
   def last_updated
     updated_at.strftime("%-d/%-m/%y %-l:%M %p")
   end
-
-  def active?
-    status == 'active'
-  end
 end
